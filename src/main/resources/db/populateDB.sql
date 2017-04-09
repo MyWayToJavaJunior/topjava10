d96af071cc492d02f651258a50ff6f46d92d37c8
lex <btln@mail.ru>
2017-04-09 18:47:26 +0300

    HW_03 Init and populate  SQL script

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER  SEQUENCE global_seq_meal RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password');
@@ -11,3 +13,14 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+
+INSERT INTO meals (date_time, description, calories)
+    VALUES
+      ('2015-05-30 10:00:00', 'завтрак', '500'),
+      ('2015-05-30 12:00:00', 'обед', '500'),
+      ('2015-05-30 20:00:00', 'ужин', '500'),
+      ('2015-05-31 10:00:00', 'завтрак', '1000'),
+      ('2015-05-31 12:00:00', 'обед', '500'),
+      ('2015-05-31 20:00:00', 'ужин', '510');
+
